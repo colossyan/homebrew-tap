b11076e2f5c9301f5d7d42051cb8a3b11946521d
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-05-07 14:46:29 +0200

    feat: 0.6-alpha.0

---
+++ Formula/cde.rb
@@ -0,0 +1,14 @@
+require "formula"
+require_relative "lib/private_strategy"
+
+class Cde < Formula
+  homepage "https://github.com/colossyan/cde"
+  url "https://github.com/colossyan/cde/releases/download/v0.6-alpha.0/cde-aarch64-apple-darwin.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "dd79ae6726ae4cded73711d426fdb9d7bce9b5ce8d413cf607d5f62002268ee5"
+  head "https://github.com/colossyan/cde.git"
+  version "0.6-alpha.0"
+
+  def install
+    bin.install "cde"
+  end
+end
