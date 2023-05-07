c09f53c4502482482f9c664147436953712ab56b
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-05-07 21:04:57 +0200

    chore: update checksum

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -4,7 +4,7 @@ require_relative "lib/private_strategy"
 class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   url "https://github.com/colossyan/cde/releases/download/v0.6-alpha.4/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "dd79ae6726ae4cded73711d426fdb9d7bce9b5ce8d413cf607d5f62002268ee5"
+  sha256 "c9a31685ee427eda3f8ccc2ce08b4116a3b139017b0e6aff1ec27927fe7f3bb6"
   head "https://github.com/colossyan/cde.git"
   version "0.6-alpha.4"
 
