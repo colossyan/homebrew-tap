598f2788a12d94969db15ed4965b21bb63a766ba
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-05-23 09:55:34 +0200

    fix: typo

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -7,7 +7,7 @@ class Cde < Formula
   version "v0.7-alpha.15"
 
   resource "cde" do
-    on_windows do
+    on_macos do
       url "https://github.com/colossyan/cde/releases/download/v0.7-alpha.13/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "fad71e6404f98fa0896163519835d552cf19129362a07506bdb62a8a5dfa0b43"
     end
