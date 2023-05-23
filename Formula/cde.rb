5debb83e709bea348dc756a77b5ea24f5ec43886
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-05-23 09:54:03 +0200

    fix: os-specific install

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -4,17 +4,24 @@ require_relative "lib/private_strategy"
 class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
-  version "v0.7-alpha.14"
+  version "v0.7-alpha.15"
 
-  def install
+  resource "cde" do
     on_windows do
       url "https://github.com/colossyan/cde/releases/download/v0.7-alpha.13/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "fad71e6404f98fa0896163519835d552cf19129362a07506bdb62a8a5dfa0b43"
-      bin.install "cde-macos" => "cde"
     end
     on_linux do
       url "https://github.com/colossyan/cde/releases/download/v0.7-alpha.13/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "69752c23a0277542cd2808ef4984dfe2c9bee35d27b497c0a0661cce8a40007c"
+    end
+  end
+
+  def install
+    if OS.mac?
+      bin.install "cde-macos" => "cde"
+    end
+    if OS.linux?
       bin.install "cde-linux" => "cde"
     end
   end
