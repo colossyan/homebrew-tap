23f82cb7ac8b980d2cde940b51f6a31f105b05e5
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-05-27 18:18:05 +0200

    fix: linux case

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -8,7 +8,7 @@ class Cde < Formula
   url "https://github.com/colossyan/cde/releases/download/v0.7-alpha.13/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "fad71e6404f98fa0896163519835d552cf19129362a07506bdb62a8a5dfa0b43"
 
-  on :linux do
+  on_linux do
     url url "https://github.com/colossyan/cde/releases/download/v0.7-alpha.13/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "69752c23a0277542cd2808ef4984dfe2c9bee35d27b497c0a0661cce8a40007c"
   end
