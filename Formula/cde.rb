4ed131c9de4a4d78565239ff1bb7e5d4c0476793
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-08-08 19:56:19 +0200

    chore: release v0.8-beta.8

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -4,13 +4,13 @@ require_relative "lib/private_strategy"
 class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
-  version "v0.8-beta.3"
-  url "https://github.com/colossyan/cde/releases/download/v0.8-beta.3/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "a89aa6196aed18d58b904af868fc494ea2ac71b233334c042f57d999dfc078f0"
+  version "v0.8-beta.8"
+  url "https://github.com/colossyan/cde/releases/download/v0.8-beta.8/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "3a1980b911805e029d1e79e91e4bc13a0a6e8f8dc4c36bba54a28a92e626db38"
 
   on_linux do
-    url url "https://github.com/colossyan/cde/releases/download/v0.8-beta.3/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "ffc6147337d46e109faa64c0ce2f80ff35ca5bccbffeab81d579fc28b438e6a7"
+    url url "https://github.com/colossyan/cde/releases/download/v0.8-beta.8/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "521af665c29faa334b0da5d271f813398b65f05f20b89795a2aa31dee98c3fe6"
   end
 
   def install
