615f6773cff9be44cde4ef9b455412c2f193b13d
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-08-09 10:09:50 +0200

    fix: install url on linux

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -9,7 +9,7 @@ class Cde < Formula
   sha256 "3a1980b911805e029d1e79e91e4bc13a0a6e8f8dc4c36bba54a28a92e626db38"
 
   on_linux do
-    url url "https://github.com/colossyan/cde/releases/download/v0.8-beta.8/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/colossyan/cde/releases/download/v0.8-beta.8/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "521af665c29faa334b0da5d271f813398b65f05f20b89795a2aa31dee98c3fe6"
   end
 
