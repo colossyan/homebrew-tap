b993540fac514e3ebbb168c5c5b087cd765e226f
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-08-10 01:29:23 +0200

    fix: download urls

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -5,11 +5,11 @@ class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
   version "0.8-beta.19"
-  url "https://github.com/colossyan/cde/releases/download/0.8-beta.19/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/colossyan/cde/releases/download/v0.8-beta.19/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "b35c2b24d58dc88781b694b43f694e6d9036b4f19969eea8a77f6920b98cf033"
 
   on_linux do
-    url "https://github.com/colossyan/cde/releases/download/0.8-beta.19/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/colossyan/cde/releases/download/v0.8-beta.19/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "d76d0607188514c9a88d4f0caaff7df7b812820de9a4dd41fe1f0089f8e88c7e"
   end
 
