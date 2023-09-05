0603cbbcd71f461755abbb19a3779d9d2f6aea11
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-09-05 14:18:49 +0200

    chore: bump v

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -5,11 +5,11 @@ class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
   version "v0.9-beta.0"
-  url "https://github.com/colossyan/cde/releases/download/v0.8-beta.26/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/colossyan/cde/releases/download/v0.9-beta.0/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "5bbe6e0f54ea7d2d4397d37e156eadb2841533a95dfbe91ab235f21f39df8da7"
 
   on_linux do
-    url "https://github.com/colossyan/cde/releases/download/v0.8-beta.26/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    url "https://github.com/colossyan/cde/releases/download/v0.9-beta.0/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "284c9af50f3bbb1b193ee686fdea9dd7a0980611b91156e6453dd07e6167098a"
   end
 
