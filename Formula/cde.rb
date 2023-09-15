75e3b745c2bd11a338aa1d8f866666a74e7a9bb8
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2023-09-15 16:46:42 +0200

    Revert "refactor: move release-specific values to separate file"
    This reverts commit ca9d95e42f0f573b4c1358491159514fdb1a4bbc.

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -1,17 +1,16 @@
 require "formula"
-require_relative "constants"
 require_relative "lib/private_strategy"
 
 class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
-  url "https://github.com/colossyan/cde/releases/download/#{VERSION}/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version VERSION
-  sha256 SHA256_MACOS
+  version "v0.9-beta.3"
+  url "https://github.com/colossyan/cde/releases/download/#{version}/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "370581b3ce75e2cc1be968695b4bed3014db76bce570bf9fddb76d3be1b0869d"
 
   on_linux do
-    url "https://github.com/colossyan/cde/releases/download/#{VERSION}/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 Constants::SHA256_LINUX
+    url "https://github.com/colossyan/cde/releases/download/#{version}/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "eb083afe218466681caa502243fed1d58af94403135e22b4ef7fa17549377e8d"
   end
 
   def install
