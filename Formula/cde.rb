3ad542460e95506234b3885cfcdceba3bd7b75bd
Szabó Patrik <47889408+nubcake94@users.noreply.github.com>
2024-08-15 12:29:59 +0200

    bumpcde

--- Formula/cde.rb
+++ Formula/cde.rb
@@ -4,13 +4,13 @@ require_relative "lib/private_strategy"
 class Cde < Formula
   homepage "https://github.com/colossyan/cde"
   head "https://github.com/colossyan/cde.git"
-  version "v0.9-beta.6"
+  version "v0.9-beta.8"
   url "https://github.com/colossyan/cde/releases/download/#{version}/cde-macos.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "a50714e027d305b646290359fdfceba7d3029452a13e4acb75d0f24d32db2c20"
+  sha256 "c70a4d55bb2b5d6fa5f080e1b44e4fc18d7ae156873ab6936f222c036529ef4d"
 
   on_linux do
     url "https://github.com/colossyan/cde/releases/download/#{version}/cde-linux.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "6935082cc57a143069a7f6b7c4b53e41fad4e0ff33527875f6f42325b011873a"
+    sha256 "473f6f5a64ddbd93931a1f474214774902be9550fb505ac8d878412662ff7e85"
   end
 
   def install
